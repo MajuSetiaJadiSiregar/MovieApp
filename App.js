81866fd575911e5fa8c4f613e17b9697a5ac371d
majusiregar@gmail.com <47182263+bangcadel@users.noreply.github.com>
2022-10-23 16:33:32 +0700

    [setup-state-movie]

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch} from 'react-redux';
 import { Text, View } from 'react-native';
+import {readMovie} from './src/features/movieSlice';
 
 const App = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(readMovie());
+  },[dispatch])
   return (
     <View>
       <Text>maju</Text>
--- src/features/movieSlice.js
+++ src/features/movieSlice.js
@@ -1,11 +1,27 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk, createEntityAdapter } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const readMovie = createAsyncThunk("movie/readMovie", async () => {
+   const response = await axios.get('https://api.themoviedb.org/3/movie/upcoming?api_key=84648c1d2dece9e696e82e12c46b738e&language=en-US&page=1');
+   console.log(response.data.results);
+   return response.data.results;
+});
+
+const movieEntity = createEntityAdapter({
+   selectId: (movie) => movie.id
+});
+
 
 const movieSlice = createSlice({
-   name : 'movie', 
-   initialState : {
-      nama : 'maju',
-      kelas : 'abc'
+   name : 'movie',
+   initialState : movieEntity.getInitialState(),
+   extraReducers : {
+      [readMovie.fulfilled] : (state, action) => {
+         movieEntity.setAll(state, action.payload);
+      }
    }
 });
 
+
+export const movieSelectors = movieEntity.getSelectors(state => state.movie);
 export default movieSlice.reducer;
